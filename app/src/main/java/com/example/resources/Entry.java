43704c5a568ff840baf634e985614397cb75fb1b
akshaytejus <akashaytejus@gmail.com>
2023-01-10 19:20:43 +0530

    login screen

--- app/src/main/java/com/example/resources/Entry.java
+++ app/src/main/java/com/example/resources/Entry.java
@@ -2,7 +2,10 @@ package com.example.resources;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class Entry extends AppCompatActivity {
 
@@ -10,5 +13,12 @@ public class Entry extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_entry);
+
+        Button login_button = findViewById(R.id.loginButton);
+
+        login_button.setOnClickListener(view -> {
+            Intent intent = new Intent(Entry.this, Login.class);
+            startActivity(intent);
+        });
     }
 }
